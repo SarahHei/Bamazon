f02cc7e0addf6b69ea5bf5abac460d5f0802254f
SarahHei <sarahhei@compasseval.com>
2019-02-09 17:59:52 -0500

    Updates

---
+++ bamazonCustomer.js
@@ -0,0 +1,76 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+  host: "127.0.0.1",
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: "bamazon"
+});
+
+connection.connect(function(err) {  
+  if (err) throw err;
+  console.log("Connected");
+  showMenu();
+});
+
+
+function showMenu() { 
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+    console.log("----------For Sale---------");
+    for (var i = 0; i < res.length; i++) {
+      console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name+ " | " + res[i].price + " | " + res[i].stock_quantity);
+    }
+    console.log("-----------------------------------");
+    wouldLikeToBuy();
+  });
+}
+
+function wouldLikeToBuy() {
+  inquirer
+    .prompt([
+      {
+      name: "askItem",
+      type: "input",
+      message: "What item number would you like to buy?",
+      },
+      {
+        name: "howMany",
+        type: "input",
+        message: "How many would you like ?"
+      }
+    
+    ])
+    .then(function(answer) {
+
+      var itemToBuy = answer.askItem; 
+      var thisMany = answer.howMany; 
+
+        var query = connection.query("SELECT * FROM products WHERE item_id=?", [itemToBuy], function(err, res) {
+          if (res[0].stock_quantity < parseInt(thisMany)) { 
+            console.log("Insufficient Quantity!");
+          } else {
+            console.log("Your Order is complete");
+            connection.query("UPDATE products SET ? WHERE ?", [
+              {
+                stock_quantity: res[0].stock_quantity - parseInt(thisMany)
+            },
+            {
+              item_id: itemToBuy
+            }
+
+          ],function(err, result) {
+            
+            console.log("The price of " + thisMany + " " + res[0].product_name + "s" + " is " +  res[0].price * parseInt(thisMany) + " dollars each.");
+
+            connection.end();
+              
+            })
+          }
+        });
+      
+    });
+    
+}
---
+++ schema.sql
@@ -0,0 +1,16 @@
+USE bamazon;
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES 
+(1, "red nail polish", "Cosmetics", $5, 100),
+(2, "red lipstick", "Cosmetics", $10, 100),
+(3, "How to Get Ahead in Advertising", "Movies", $12,99, 10),
+(4, "Barbarella", "Movies", $12.99, 10),
+(5, "Zen and the Art of Motorcycle Maintenance", "Books", $10, 50),
+(6, "Gone with the Wind", "Books", $7.99, 500),
+(7, "light bulbs", "Housewares", $3.99, 100),
+(8, "tin foil", "Housewares", $1.99, 100),
+(9, "socks", "Clothing", $5, 200),
+(10, "t-shirts", "Clothing", $10, 100);
+
+SELECT * FROM products;
